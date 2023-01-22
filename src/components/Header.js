0a41fac913bc89315c6e4a41bebd9d2ec4d8001f
ArtomMrdn <artommrdn47@gmail.com>
2023-01-22 23:10:46 +0200

    adaptive added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,8 @@
 function Header() {
   return (
-    <nav className='black darken-2'>
-      <div className='nav-wrapper'>
-        <a href='#' className='brand-logo'>
-          Dummy Shop
-        </a>
-        <ul id='nav-mobile' className='right hide-on-med-and-down'></ul>
-      </div>
-    </nav>
+    <div className='header'>
+      <a>Dummy Shop</a>
+    </div>
   );
 }
 
