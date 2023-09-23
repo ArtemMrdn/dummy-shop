a4ee4b120efff8b3c3bf0c10590a846e8bfd4055
ArtomMrdn <artommrdn47@gmail.com>
2023-09-23 12:57:19 +0300

    Readme updated

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is a small store that will develop in the future, now the store has a list of products stylized as cards with different categories.
 
-The project is deployed here https://artemmrdn.github.io/dummy-shop/
+The project is deployed [here](https://artemmrdn.github.io/dummy-shop/).
--- src/components/Products.js
+++ src/components/Products.js
@@ -18,3 +18,23 @@ function Products(props) {
 }
 
 export default Products;
+
+for (let i = 1; i <= 5; i++) {
+  document.getElementById(
+    "w__stars"
+  ).innerHTML += `<span class="r__icon">[ ]</span>`;
+}
+var icon = document.getElementsByClassName("r__icon");
+
+for (let i = 0; i < icon.length; i++) {
+  icon[i].addEventListener("click", function (e) {
+    console.log("--");
+    for (let j = 0; j < i + 1; j++) {
+      console.log("i: " + i);
+      console.log("j: " + (j + 1));
+      console.log("Rest: " + (j + (5 - (i + 1))));
+      icon[j].innerHTML = `[X]`;
+      icon[i + (5 - (i + 1))].innerHTML = `[ ]`;
+    }
+  });
+}
